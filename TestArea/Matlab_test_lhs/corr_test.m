2061cfcefdb04fb6e78b88872a53651c28225e66
Christoffer Eduards <christoffer.eduards@gmail.com>
2020-04-14 22:33:01 +0200

    matlab implementation of correlation matrix

---
+++ TestArea/Matlab_test_lhs/corr_test.m
@@ -0,0 +1,22 @@
+test = [5 2 3 1 2 3; 10 5 4 4 5 6; 15 10 5 3 2 1; 20 3 1 4 5 6; 25 2 3 222 1 3; 30 1 2 444 5 123];
+corrm = corr(test)
+
+n = size(test,2);
+corrm_test = zeros(n,n);
+for i = 1:n
+    for j = 1:n
+        if (i == j)
+            corrm_test(i,j) = 1;
+        else
+            corrm_test(i,j) = corr_egen(test(:,i),test(:,j));
+        end
+    end
+end
+
+corrm_test
+
+function rho = corr_egen(X,Y)
+    X_hat = mean(X);
+    Y_hat = mean(Y);
+    rho = (sum((X - X_hat).*(Y - Y_hat))) / (sqrt(sum((X - X_hat).^2)) * sqrt(sum((Y - Y_hat).^2)));         
+end
