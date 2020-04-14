c530742d6a82befc9cc9bd9bb66199407d4e0238
Christoffer Eduards <christoffer.eduards@gmail.com>
2020-04-14 10:48:36 +0200

    random num sim

--- TestArea/LHSD/lhs_regular/lhs_regular/lhs_regular.cpp
+++ TestArea/LHSD/lhs_regular/lhs_regular/lhs_regular.cpp
@@ -1,34 +1,62 @@
 #include <iostream>
 
-//Boost-test
-//#include <boost/date_time/gregorian/gregorian.hpp>
-//namespace bg = boost::gregorian;
-//void boost_test();
+//Boost packages for numeric represenation
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <boost/numeric/ublas/io.hpp>
 
 
+//Package to generate random variables
+#include <random>
+
+using namespace boost::numeric::ublas;
+
+
+matrix<double> GC_Sim(matrix<double> corr, int n);
+vector<double> gen_normal(double m, double s, int n);
 
 
 int main() {
-    std::cout << "hejs";
-    //boost_test();
+    
+    gen_normal(0, 1, 10);
+
+    
+}
+
+
+matrix<double> GC_Sim(matrix<double> corr, int n) {
+    int dim = corr.size1(); // Number of risk factors
+    matrix<double> U(dim, n); // Return matrix
+    
+    //Generate a matrix with random numbers
+    matrix<double> y(dim, n);
+
+    //for (auto values_iter = gen_normal(0,1,n).begin(); values_iter != gen_normal(0,1,n).end(); ++values_iter, ++iter1) 
+    
+    //std::copy(gen_normal(0, 1, n).begin(), gen_normal(0, 1, n).end(), y.begin1());
+    
+    gen_normal(0, 1, n);
+
+    return U;
 }
 
 
 
 
+//Generate n normal variables with mean m and standard deviation s
+vector<double> gen_normal(double m, double s, int n) {
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(m, s);
+    vector<double> rand(n);
+        
+    for (int i = 0; i < n; ++i) {
+        double number = distribution(generator);
+        rand[i] = number;
+        //std::cout << rand[i] << std::endl;
+    }
+    
+    return rand;
+}
 
-//Boost-test
-/*void boost_test() {
-    // date as simple ISO string
-    std::string stringDate("20191110");
-    bg::date todayDate(bg::from_undelimited_string(stringDate));
-    bg::date::ymd_type ymdDate = todayDate.year_month_day();
-    bg::greg_weekday weekdayDate = todayDate.day_of_week();
-    std::cout
-        << "Tutorial made with love from Badprog.com :D on "
-        << weekdayDate.as_long_string() << " "
-        << ymdDate.day << " "
-        << ymdDate.month.as_long_string() << " "
-        << ymdDate.year
-        << std::endl;
-}*/
+
+
