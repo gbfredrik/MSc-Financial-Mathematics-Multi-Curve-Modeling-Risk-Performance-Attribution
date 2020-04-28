428bbffb2f592cc410bb109fa2ef5903930d53f0
Fredrik Gerdin <fredrik.gerdin.borjesson@hotmail.com>
2020-04-28 18:24:00 +0200

    Created test of QR PCA/SVD

---
+++ TestArea/MatlabTestQRSVD.m
@@ -0,0 +1,32 @@
+% Från Freddans exempel typ
+%m = normrnd(0, 1, 10950, 1500);
+%[Q, R, E] = qr(m, 0);
+%RR = R' * R;
+%[U, S, V] = svd(RR, 0);
+
+
+%%
+clear
+H = normrnd(0, 1, 10950, 1500);
+%%
+tic
+[Q1, R1] = qr(H, 0);
+
+[U1, D1, V] = svd(R1, 0);
+
+EVec = Q1 * V(:,1:24);
+EVal = diag(D1).^2;
+EVal = EVal(1:24,1);
+toc
+
+%%
+tic
+%Cv = H * H';
+%[Uc, Dc, Vc] = svd(Cv);
+[Uc, Dc, Vc] = svds(H, 24);
+EValc = diag(Dc).^2;
+toc
+
+%%
+errorEVec = norm(Uc(:,1:24) - EVec) % Near 0, awkward rounding error
+ErrorEVal = norm(EValc - EVal) % Approx. 0
