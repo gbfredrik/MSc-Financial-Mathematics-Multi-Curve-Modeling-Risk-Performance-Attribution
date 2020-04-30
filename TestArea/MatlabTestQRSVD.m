50baf4fc47599e3f2e274864dd1e7294f2b44d7d
Fredrik Gerdin <fredrik.gerdin.borjesson@hotmail.com>
2020-04-30 17:39:14 +0200

    Minor testing

--- TestArea/MatlabTestQRSVD.m
+++ TestArea/MatlabTestQRSVD.m
@@ -11,22 +11,22 @@ H = normrnd(0, 1, 10950, 1500);
 %%
 tic
 [Q1, R1] = qr(H, 0);
-
 [U1, D1, V] = svd(R1, 0);
 
-EVec = Q1 * V(:,1:24);
+EVec = Q1 * V(:,1:6);
 EVal = diag(D1).^2;
-EVal = EVal(1:24,1);
+EVal = EVal(1:6,1);
 toc
 
 %%
 tic
 %Cv = H * H';
 %[Uc, Dc, Vc] = svd(Cv);
-[Uc, Dc, Vc] = svds(H, 24);
+[Uc, Dc, Vc] = svds(H, 6);
+EVecc = Uc(:,1:6);
 EValc = diag(Dc).^2;
 toc
 
 %%
-errorEVec = norm(Uc(:,1:24) - EVec) % Near 0, awkward rounding error
+errorEVec = norm(EVecc - EVec) % Near 0, awkward rounding error
 ErrorEVal = norm(EValc - EVal) % Approx. 0
