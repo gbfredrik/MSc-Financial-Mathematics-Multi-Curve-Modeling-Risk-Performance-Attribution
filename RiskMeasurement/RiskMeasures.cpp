3be4d60d8b77ae6b3ecf998c57fe2b112fe23f53
Fredrik Gerdin <fredrik.gerdin.borjesson@hotmail.com>
2020-06-11 16:46:26 +0200

    Added final functions and cleaned. Needs testing with real data later.

--- RiskMeasurement/RiskMeasures.cpp
+++ RiskMeasurement/RiskMeasures.cpp
@@ -12,48 +12,48 @@ using namespace boost::numeric;
 
 // --- VaR ---
 double RiskMeasures::VaR(
-    ublas::vector<double> const& PnL,
+    ublas::vector<double> const& outcomes,
     double const c
 ) {
-    ublas::vector<double> sorted_PnL(-PnL);
-    std::sort(sorted_PnL.begin(), sorted_PnL.end());
+    ublas::vector<double> sorted_outcomes(-outcomes);
+    std::sort(sorted_outcomes.begin(), sorted_outcomes.end());
 
-    int index_VaR{ VaR_index(c, sorted_PnL.size()) };
+    int index_VaR{ VaR_index(c, sorted_outcomes.size()) };
 
-    return sorted_PnL(index_VaR);
+    return sorted_outcomes(index_VaR);
 }
 
-ublas::vector<double> RiskMeasures::VaR_series(
-    ublas::vector<double>& PnL,
-    double const c,
-    int const window
-) {
-    size_t n{ PnL.size() };
-    ublas::vector<double> VaR_measures(n - window);
-
-    for (size_t i{ 0 }; i < n - window; ++i) {
-        VaR_measures(i) = VaR(
-            ublas::vector_range<ublas::vector<double>> (
-                PnL,
-                ublas::range(i, i + window)
-            ), 
-            c
-        );
-    }
-    
-    return VaR_measures;
-}
+//ublas::vector<double> RiskMeasures::VaR_series(
+//    ublas::vector<double>& outcomes,
+//    double const c,
+//    int const window
+//) {
+//    size_t n{ outcomes.size() };
+//    ublas::vector<double> VaR_measures(n - window);
+//
+//    for (size_t i{ 0 }; i < n - window; ++i) {
+//        VaR_measures(i) = VaR(
+//            ublas::vector_range<ublas::vector<double>> (
+//                outcomes,
+//                ublas::range(i, i + window)
+//            ), 
+//            c
+//        );
+//    }
+//    
+//    return VaR_measures;
+//}
 
 // --- VaR Backtesting---
 // VaR hypothesis backtesting
 bool RiskMeasures::VaR_hypothesis_test(
-    ublas::vector<double> const& VaR, 
-    ublas::vector<double> const& PnL, 
+    ublas::vector<double> const& VaRs, 
+    ublas::vector<double> const& PnLs, 
     double const c,
     double const alpha
 ) {
-    int X_T{ test_statistic_X_T(VaR, PnL) };
-    int T{ static_cast<int>(VaR.size()) };
+    int X_T{ test_statistic_X_T(VaRs, PnLs) };
+    int T{ static_cast<int>(VaRs.size()) };
     double p{ 1 - c };
     double Z{ test_statistic_Z(X_T, T, p) };
     double tilde_m{ statisticsOperations::invCDFNorm(1 - alpha) }; // = invCDFNorm( 1 - alpha, 0.0, 1.0);
@@ -62,10 +62,10 @@ bool RiskMeasures::VaR_hypothesis_test(
 }
 
 int RiskMeasures::test_statistic_X_T(
-    ublas::vector<double> const& VaR, 
-    ublas::vector<double> const& PnL
+    ublas::vector<double> const& VaRs, 
+    ublas::vector<double> const& PnLs
 ) {
-    return sum(VaR_breaches(VaR, PnL));
+    return sum(VaR_breaches(VaRs, PnLs));
 }
 
 double RiskMeasures::test_statistic_Z(int const X_T, int const T, double const p) {
@@ -74,15 +74,15 @@ double RiskMeasures::test_statistic_Z(int const X_T, int const T, double const p
 
 // VaR Christoffersen's test
 bool RiskMeasures::VaR_Christoffersen_test(
-    ublas::vector<double> const& VaR, 
-    ublas::vector<double> const& PnL, 
+    ublas::vector<double> const& VaRs, 
+    ublas::vector<double> const& PnLs, 
     double const alpha
 ) {
     double u_00{ 0.0 };
     double u_01{ 0.0 };
     double u_10{ 0.0 };
     double u_11{ 0.0 };
-    number_of_periods(VaR, PnL, u_00, u_01, u_10, u_11);
+    number_of_periods(VaRs, PnLs, u_00, u_01, u_10, u_11);
     
     double pi{ (u_01 + u_11) / (u_00 + u_01 + u_10 + u_11) };
     double pi_01{ (u_01) / (u_00 + u_01) };
@@ -106,16 +106,16 @@ bool RiskMeasures::VaR_Christoffersen_test(
 }
 
 void RiskMeasures::number_of_periods(
-    ublas::vector<double> const& VaR,
-    ublas::vector<double> const& PnL,
+    ublas::vector<double> const& VaRs,
+    ublas::vector<double> const& PnLs,
     double& u_00,
     double& u_01,
     double& u_10,
     double& u_11
 ) {
-    ublas::vector<int> indicator(VaR_breaches(VaR, PnL));
+    ublas::vector<int> indicator(VaR_breaches(VaRs, PnLs));
     
-    for (size_t i{ 0 }, n{ VaR.size() - 1}; i < n; ++i) {
+    for (size_t i{ 0 }, n{ VaRs.size() - 1}; i < n; ++i) {
         bool exceed_current{ indicator(i) == 1};
         bool exceed_next{ indicator(i + 1) == 1};
 
@@ -146,63 +146,85 @@ double RiskMeasures::test_statistic_christoffersen(
 
 // --- ES ---
 double RiskMeasures::ES(
-    ublas::vector<double> const& PnL,
+    ublas::vector<double> const& outcomes,
     double const c
 ) {
-    ublas::vector<double> sorted_PnL(-PnL);
-    std::sort(sorted_PnL.begin(), sorted_PnL.end());
+    ublas::vector<double> sorted_outcomes(-outcomes);
+    std::sort(sorted_outcomes.begin(), sorted_outcomes.end());
 
-    int index_VaR{ VaR_index(c, sorted_PnL.size()) };
+    int index_VaR{ VaR_index(c, sorted_outcomes.size()) };
 
     ublas::vector_range<ublas::vector<double>> exceedances(
-        sorted_PnL,
-        ublas::range(index_VaR, sorted_PnL.size())
+        sorted_outcomes,
+        ublas::range(index_VaR, sorted_outcomes.size())
     );
 
     return matrixOperations::vector_average(exceedances);
 }
 
-ublas::vector<double> RiskMeasures::ES_series(
-    ublas::vector<double>& PnL,
-    double const c,
-    int const window
-) {
-    size_t n{ PnL.size() };
-    ublas::vector<double> ES_measures(n - window);
-
-    for (size_t i{ 0 }; i < n - window; ++i) {
-        ES_measures(i) = ES(
-            ublas::vector_range<ublas::vector<double>>(
-                PnL,
-                ublas::range(i, i + window)
-            ),
-            c
-        );
-    }
-
-    return ES_measures;
-}
+//ublas::vector<double> RiskMeasures::ES_series(
+//    ublas::vector<double>& PnLs,
+//    double const c,
+//    int const window
+//) {
+//    size_t n{ PnLs.size() };
+//    ublas::vector<double> ES_measures(n - window);
+//
+//    for (size_t i{ 0 }; i < n - window; ++i) {
+//        ES_measures(i) = ES(
+//            ublas::vector_range<ublas::vector<double>>(
+//                PnLs,
+//                ublas::range(i, i + window)
+//            ),
+//            c
+//        );
+//    }
+//
+//    return ES_measures;
+//}
 
 // --- ES Backtesting ---
 // Test 1: Acerbi and Szekely
 
 bool RiskMeasures::ES_Acerbi_Szekely(
-    ublas::vector<double> const& VaR, 
-    ublas::vector<double> const& ES, 
-    ublas::vector<double> const& PnL, 
-    double const c
+    ublas::matrix<double> const& X,
+    ublas::vector<double> const& VaRs, 
+    ublas::vector<double> const& ESs, 
+    ublas::vector<double> const& PnLs, 
+    double const phi
 ) {
-    size_t T{ VaR.size() };
-    ublas::vector<int> indicator(VaR_breaches(VaR, PnL));
-    
+    size_t M{ X.size2() }; // columns
+    ublas::vector<double> Z(M);
+    double Z_X_actual{ test_statistic_Z1(VaRs, ESs, PnLs) };
+    int n{ 0 };
+
+    for (size_t i{ 0 }; i < M; ++i) {
+        Z(i) = test_statistic_Z1(VaRs, ESs, column(X, i)); // Todo: Control
+        if (Z(i) < Z_X_actual) {
+            ++n;
+        }
+    }
+
+    double p{ n / static_cast<double>(M) };
+
+    return p < phi;
+}
+
+double RiskMeasures::test_statistic_Z1(
+    ublas::vector<double> const& VaRs, 
+    ublas::vector<double> const& ESs, 
+    ublas::vector<double> const& PnLs
+) {
+    ublas::vector<int> indicator(VaR_breaches(VaRs, PnLs));
+
     int N_T{ sum(indicator) };
     if (N_T == 0) { // Exit if no breaches to avoid div. by zero in Z_X.
         return 0;
     }
 
-    double Z_X{ sum(element_div(element_prod(PnL, indicator), ES)) / N_T + 1.0 };
-    std::cout << "Z_X: " << Z_X << std::endl;
-    return Z_X < 0;
+    double Z_X{ sum(element_div(element_prod(PnLs, indicator), ESs)) / N_T + 1.0 };
+    
+    return Z_X;
 }
 
 // Kernel Density Estimator
@@ -214,14 +236,14 @@ int RiskMeasures::VaR_index(double const c, size_t const n) {
 }
 
 ublas::vector<double> RiskMeasures::VaR_breaches(
-    ublas::vector<double> const& VaR, 
-    ublas::vector<double> const& PnL
+    ublas::vector<double> const& VaRs, 
+    ublas::vector<double> const& PnLs
 ) {
-    size_t T{ VaR.size() };
+    size_t T{ VaRs.size() };
     ublas::vector<double> indicator(T);
 
     for (size_t i{ 0 }; i < T; ++i) {
-        indicator(i) = -VaR(i) > PnL(i);
+        indicator(i) = -VaRs(i) > PnLs(i);
     }
     
     return indicator;
--- RiskMeasurement/riskMeasureTester.cpp
+++ RiskMeasurement/riskMeasureTester.cpp
@@ -1,75 +1,71 @@
-#include "RiskMeasures.h"
-
-#include "../MathLibrary/rvSim.h"
-#include "../CurveLibrary/sample_handler.h"
-
-#include <boost/numeric/ublas/matrix.hpp>
-#include <boost/numeric/ublas/matrix_proxy.hpp>
-#include <boost/numeric/ublas/vector_proxy.hpp>
-#include <boost/numeric/ublas/io.hpp>
-
-#include <iostream>
-#include <windows.h>
-#include <winuser.h>
-
-using namespace boost::numeric;
-
-int main() {
-    //ublas::vector<double> test_losses(2000);
-    //test_losses = row(rvSim::gen_normal(1, 2000), 0);
-    //write_csv_vector(test_losses, "test_losses.csv");
-    double c{ 0.95 };
-    int window{ 252 };
-
-    ublas::vector<double> returns( read_csv_vector("returns.csv") );
-
-    //std::cout << test_losses << "\n";
-
-    std::cout << "VaR full: " << RiskMeasures::VaR(returns, c) << std::endl;
-    std::cout << "ES full: " << RiskMeasures::ES(returns, c) << std::endl;
-
-    ublas::vector<double> VaRs(RiskMeasures::VaR_series(returns, c, window));
-    ublas::vector<double> ESs(RiskMeasures::ES_series(returns, c, window));
-    //std::cout << VaRs << std::endl;
-    ublas::vector_range<ublas::vector<double>> PnLs(
-        returns, 
-        ublas::range(window, returns.size())
-    );
-
-    std::cout << "Returns: " << returns.size() << std::endl;
-    std::cout << "VaRs: " << VaRs.size() << std::endl;
-    std::cout << "ESs: " << ESs.size() << std::endl;
-    std::cout << "PnLs: " << PnLs.size() << std::endl << std::endl;
-    
-    std::cout << "VaR Backtest:" << std::endl;
-    bool VaR_hyp{ RiskMeasures::VaR_hypothesis_test(
-        VaRs,
-        PnLs,
-        c,
-        0.05
-    ) };
-    std::cout << "Reject H_0 for H_1: " << VaR_hyp << std::endl << std::endl;
-    
-    std::cout << "Christoffersen: " << RiskMeasures::VaR_Christoffersen_test(
-        VaRs,
-        ublas::vector_range<ublas::vector<double>>(
-            returns,
-            ublas::range(window, returns.size())
-            ),
-        0.01
-    ) << std::endl << std::endl;
-
-    std::cout << "ES Backtest:" << std::endl;
-    std::cout << "Acerbi & Szekely, Reject H_0 for H_1: ";
-    if (VaR_hyp) {
-        std::cout << "Rejected due to VaR rejection." << std::endl;
-    } else {
-        std::cout << RiskMeasures::ES_Acerbi_Szekely(VaRs, ESs, PnLs, 0.01) << std::endl;
-    }
-
-    write_csv_vector(VaRs, "VaRs.csv");
-    write_csv_vector(ESs, "ESs.csv");
-    write_csv_vector(PnLs, "PnLs.csv");
-
-    //MessageBox(NULL, "Backtest finished.", "Status", MB_OK);
-}
+//#include "RiskMeasures.h"
+//
+//#include "../MathLibrary/rvSim.h"
+//#include "../CurveLibrary/sample_handler.h"
+//
+//#include <boost/numeric/ublas/matrix.hpp>
+//#include <boost/numeric/ublas/matrix_proxy.hpp>
+//#include <boost/numeric/ublas/vector_proxy.hpp>
+//#include <boost/numeric/ublas/io.hpp>
+//
+//#include <iostream>
+//
+//using namespace boost::numeric;
+//
+//int main() {
+//    //ublas::vector<double> test_losses(2000);
+//    //test_losses = row(rvSim::gen_normal(1, 2000), 0);
+//    //write_csv_vector(test_losses, "test_losses.csv");
+//    double c{ 0.95 };
+//    int window{ 252 };
+//
+//    ublas::vector<double> returns( read_csv_vector("returns.csv") );
+//
+//    //std::cout << test_losses << "\n";
+//
+//    std::cout << "VaR full: " << RiskMeasures::VaR(returns, c) << std::endl;
+//    std::cout << "ES full: " << RiskMeasures::ES(returns, c) << std::endl;
+//
+//    ublas::vector<double> VaRs(RiskMeasures::VaR_series(returns, c, window));
+//    ublas::vector<double> ESs(RiskMeasures::ES_series(returns, c, window));
+//    //std::cout << VaRs << std::endl;
+//    ublas::vector_range<ublas::vector<double>> PnLs(
+//        returns, 
+//        ublas::range(window, returns.size())
+//    );
+//
+//    std::cout << "Returns: " << returns.size() << std::endl;
+//    std::cout << "VaRs: " << VaRs.size() << std::endl;
+//    std::cout << "ESs: " << ESs.size() << std::endl;
+//    std::cout << "PnLs: " << PnLs.size() << std::endl << std::endl;
+//    
+//    std::cout << "VaR Backtest:" << std::endl;
+//    bool VaR_hyp{ RiskMeasures::VaR_hypothesis_test(
+//        VaRs,
+//        PnLs,
+//        c,
+//        0.05
+//    ) };
+//    std::cout << "Reject H_0 for H_1: " << VaR_hyp << std::endl << std::endl;
+//    
+//    std::cout << "Christoffersen: " << RiskMeasures::VaR_Christoffersen_test(
+//        VaRs,
+//        ublas::vector_range<ublas::vector<double>>(
+//            returns,
+//            ublas::range(window, returns.size())
+//            ),
+//        0.01
+//    ) << std::endl << std::endl;
+//
+//    std::cout << "ES Backtest:" << std::endl;
+//    std::cout << "Acerbi & Szekely, Reject H_0 for H_1: ";
+//    if (VaR_hyp) {
+//        std::cout << "Rejected due to VaR rejection." << std::endl;
+//    } else {
+//        std::cout << "Todo: Check" << std::endl; // RiskMeasures::ES_Acerbi_Szekely(X, VaRs, ESs, PnLs, 0.99) << std::endl;
+//    }
+//
+//    write_csv_vector(VaRs, "VaRs.csv");
+//    write_csv_vector(ESs, "ESs.csv");
+//    write_csv_vector(PnLs, "PnLs.csv");
+//}
